a6295ee8d66a981c97763946cb765e8946833430
CosminIordache <cosminionutiordache@gmail.com>
2022-05-03 12:09:20 +0200

    Segunda rama

--- src/com/company/Primos.java
+++ src/com/company/Primos.java
@@ -1,35 +1,36 @@
 package com.company;
-
-
+import java.util.Scanner;
 public class Primos {
     // Generar números primos de 1 a max
-
     public static int[] generarPrimos (int max) {
         int i,j;
         if (max >= 2) {
             // Declaraciones
-            int tamañoNumeros = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[tamañoNumeros];
+            int tamañoNumero = max + 1; // Tamaño del array
+            boolean[] esPrimo = new boolean[tamañoNumero];
             // Inicializar el array
-            for (i=0; i<tamañoNumeros; i++) {
+            for (i=0; i<tamañoNumero; i++) {
                 esPrimo[i] = true;
             }
             // Eliminar el 0 y el 1, que no son primos
-            eliminarZerosYUnos(esPrimo);
+            EliminarZeroYUnos(esPrimo);
             // Criba
-            for (i=2; i<Math.sqrt(tamañoNumeros)+1; i++) {
-                eliminarMultiplosDeUno(i, tamañoNumeros, esPrimo);
+            for (i=2; i<Math.sqrt(tamañoNumero)+1; i++) {
+                if (esPrimo[i]) {
+                    // Eliminar los múltiplos de i
+                    eliminarMultiplosDe1(i, tamañoNumero, esPrimo);
+                }
             }
             // ¿Cuántos primos hay?
             int contador = 0;
-            for (i=0; i<tamañoNumeros; i++) {
+            for (i=0; i<tamañoNumero; i++) {
                 if (esPrimo[i]) {
                     contador++;
                 }
             }
             // Rellenar el vector de números primos
             int[] primos = new int[contador];
-            rellenarVector(tamañoNumeros, esPrimo, primos);
+            rellenarVector(tamañoNumero, esPrimo, primos);
             return primos;
         }
         else { // max < 2
@@ -38,34 +39,28 @@ public class Primos {
         }
     }
 
+    private static int[] vaciarVector() {
+        return new int[0];
+    }
 
-    private static void rellenarVector(int tamañoNumeros, boolean[] esPrimo, int[] primos) {
+    private static void rellenarVector(int tamañoNumero, boolean[] esPrimo, int[] primos) {
         int j;
         int i;
-        for (i=0, j=0; i< tamañoNumeros; i++) {
+        for (i=0, j=0; i< tamañoNumero; i++) {
             if (esPrimo[i]) {
                 primos[j++] = i;
             }
         }
     }
 
-
-    private static void eliminarMultiplosDeUno(int i, int tamañoNumeros, boolean[] esPrimo) {
+    private static void eliminarMultiplosDe1(int i, int tamañoNumero, boolean[] esPrimo) {
         int j;
-        if (esPrimo[i]) {
-            // Eliminar los múltiplos de i
-            for (j = 2 * i; j < tamañoNumeros; j += i) {
-                esPrimo[j] = false;
-            }
+        for (j = 2 * i; j < tamañoNumero; j += i) {
+            esPrimo[j] = false;
         }
     }
 
-    static void eliminarZerosYUnos(boolean[] esPrimo) {
+    private static void EliminarZeroYUnos(boolean[] esPrimo) {
         esPrimo[0] = esPrimo[1] = false;
-    }
-
-
-    private static int[] vaciarVector() {
-        return new int[0];
     }
 }
