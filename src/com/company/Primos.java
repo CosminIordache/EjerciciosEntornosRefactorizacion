4f9404b26335a4da1a716d4d7a3d5064f700abdf
CosminIordache <cosminionutiordache@gmail.com>
2022-05-03 11:59:39 +0200

    Segunda rama

--- src/com/company/Primos.java
+++ src/com/company/Primos.java
@@ -1,47 +1,97 @@
 package com.company;
-import java.util.Scanner;
+
+/**
+ * El programa genera los numeros primos del 1 al max
+ * @author Cosmin
+ * @version 1.1.1
+ */
 public class Primos {
     // Generar números primos de 1 a max
+
+    /**
+     * Calcula la cantidad del primos que tiene un vector
+     * @param max Es el numero maximo de primos hasta el que tiene que generar
+     * @return Devuelve los numeros primos generados
+     */
     public static int[] generarPrimos (int max) {
         int i,j;
         if (max >= 2) {
             // Declaraciones
-            int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
+            int tamañoNumeros = max + 1; // Tamaño del array
+            boolean[] esPrimo = new boolean[tamañoNumeros];
             // Inicializar el array
-            for (i=0; i<dim; i++) {
+            for (i=0; i<tamañoNumeros; i++) {
                 esPrimo[i] = true;
             }
             // Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
+            eliminarZerosYUnos(esPrimo);
             // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j = 2 * i; j < dim; j += i) {
-                        esPrimo[j] = false;
-                    }
-                }
+            for (i=2; i<Math.sqrt(tamañoNumeros)+1; i++) {
+                eliminarMultiplosDeUno(i, tamañoNumeros, esPrimo);
             }
             // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i=0; i<dim; i++) {
+            int contador = 0;
+            for (i=0; i<tamañoNumeros; i++) {
                 if (esPrimo[i]) {
-                    cuenta++;
+                    contador++;
                 }
             }
             // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i=0, j=0; i<dim; i++) {
-                if (esPrimo[i]) {
-                    primos[j++] = i;
-                }
-            }
+            int[] primos = new int[contador];
+            rellenarVector(tamañoNumeros, esPrimo, primos);
             return primos;
         }
         else { // max < 2
-            return new int[0];
+            return vaciarVector();
             // Vector vacío
         }
+    }
+
+    /**
+     * Rellena el vector
+     * @param tamañoNumeros El tamano del vector
+     * @param esPrimo Indica que numeros son primos y cuales no
+     * @param primos Todos los numeros del vector
+     */
+    private static void rellenarVector(int tamañoNumeros, boolean[] esPrimo, int[] primos) {
+        int j;
+        int i;
+        for (i=0, j=0; i< tamañoNumeros; i++) {
+            if (esPrimo[i]) {
+                primos[j++] = i;
+            }
+        }
+    }
+
+    /**
+     * Elimina los multiplos de 1
+     * @param i Recorre el vector
+     * @param tamañoNumeros
+     * @param esPrimo
+     */
+    private static void eliminarMultiplosDeUno(int i, int tamañoNumeros, boolean[] esPrimo) {
+        int j;
+        if (esPrimo[i]) {
+            // Eliminar los múltiplos de i
+            for (j = 2 * i; j < tamañoNumeros; j += i) {
+                esPrimo[j] = false;
+            }
+        }
+    }
+
+    /**
+     * Elimina el 0 y el 1 al no ser primos
+     * @param esPrimo
+     */
+    private static void eliminarZerosYUnos(boolean[] esPrimo) {
+        esPrimo[0] = esPrimo[1] = false;
+    }
+
+    /**
+     * Vacia el vector
+     * @return Devuelve un vector vacio
+     */
+    private static int[] vaciarVector() {
+        return new int[0];
     }
 }
